0ae60293b223ed31a4ee73d91a76e867adb1553e
WP-Developer <55971399+wp-deepak@users.noreply.github.com>
2021-12-16 17:40:00 +0530

    Update functions.php

--- acf-gutenberg-blocks/functions.php
+++ acf-gutenberg-blocks/functions.php
@@ -1,5 +1,43 @@
 <?php
 
+
+
+
+/*
+* Loading multiapal files using array form in wordpress.
+*
+*/
+
+$file_includes = array(
+	'includes/setup.php',           // Basic theme setup
+	'includes/scripts.php',         // Enqeue theme styles and scripts
+	'includes/widgets.php',         // Theme widget area
+	'includes/project.php',         // Custom functions for this specific project
+	'includes/acf.php',             // Advanced custom fields functions
+	'includes/blocks.php',          // Custom Gutenberg blocks
+	'includes/cpt.php',             // Custom post types setup
+	'includes/custom.php',          // Custom functions
+	'includes/ajax.php',            // Ajax related functions
+	'includes/browsers.php',        // Browser detection function
+        'includes/pagination.php',      // Ajax Pagination
+
+);
+
+/**
+ * Checks if any file have error while including it.
+ */
+foreach ( $file_includes as $file ) {
+	if ( ! $filepath = locate_template( $file ) ) {
+		trigger_error( sprintf( __( 'Error locating %s for inclusion', 'ptc_td' ), $file ), E_USER_ERROR );
+	}
+	require_once $filepath;
+}
+unset( $file, $filepath );
+global $browser;
+$browser = new Browsersphp\Browsers();
+
+
+
 /**
  * Functions for ACF Blocks
  * NOTE :- You can also wirte these code seprated file like include/block.php
